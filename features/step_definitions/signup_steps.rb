2e34caaf04190140f62695c7716a1e30309b9b5a
Krišjānis Bergmanis <krisjanis.bergmanis@testdevlab.com>
2018-04-25 21:55:27 +0300

    Locate elements by css and xpath

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,3 +1,32 @@
 When(/^I am on Appimation home page/) do
   visit('/')
+  find(:css, '#start_button')
+  find(:xpath,'//button[@id = "start_button"]')
+  
+  find(:css, '#signup-b')
+  find(:xpath,'//button[@id = "signup"]')
+  
+  find(:css, 'section#cta')
+  find(:xpath,'//section[@id = "cta"]')
+  
+  find(:css, 'input#name')
+  find(:xpath,'//input[@id = "name"]')
+  
+  find(:css, 'input#email')
+  find(:xpath,'//input[@id = "email"]')
+
+  find(:css, 'textarea#contactus-message')
+  find(:xpath,'//textarea[@id = "contactus-message"]')
+
+  find(:css, 'input#contactus-button')
+  find(:xpath,'//input[@id = "contactus-button"]')
+
+  find(:css, 'a.fa-facebook')
+  find(:xpath,'//a[@class = "icon fa-facebook"]')
+
+  find(:css, 'div.features-row')
+  find(:xpath,'//div[@class = "features-row"')
+
+  find(:css, 'div.features-row:first-of-type section:first-of-type')
+  find(:xpath,'//img[@src = "static/images/features/chain.png"]/parent::section')
 end
